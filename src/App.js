96e69f8e6e89377daacd87dce9c1180f63aa7b2d
Tan Keller <ersoichso@gmail.com>
2019-09-29 13:47:45 +0200

    Add Home page to Appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Router } from '@reach/router';
+
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Home path="/" />
+      </Router>
       
     </div>
   );
