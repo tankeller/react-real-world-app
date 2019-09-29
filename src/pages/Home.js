bda789a5fc4be26773c9a4dfe6f73757d480c7ec
Tan Keller <ersoichso@gmail.com>
2019-09-29 14:15:43 +0200

    Add FeedToggle to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
+import FeedToggle from '../components/feedToggle/FeedToggle';
+
 const Home = () => {
     return (
-        <h1>Home!</h1>
+        <div class="home-page">
+            <div class="banner">
+                <div class="container">
+                    <h1 class="logo-font">conduit</h1>
+                    <p>A place to share your knowledge.</p>
+                </div>
+            </div>
+
+            <div class="container page">
+                <div class="row">
+                    <div class="col-md-9">
+                        <FeedToggle />
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
     )
 }
 
