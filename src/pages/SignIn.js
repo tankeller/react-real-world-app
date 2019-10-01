7b94e6a1ae0f29c7c6ca460105a3aba7dd114e5a
Tan Keller <ersoichso@gmail.com>
2019-10-01 21:47:32 +0200

    Add content to signin page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from '@reach/router';
+
+import ErrorMessage from '../components/errorMessage/ErrorMessage';
 
 const SignIn = () => {
+
+    const [errorMessage] = useState("");
+
     return (
-        <h1>SignInPage</h1>
+        <div className="auth-page">
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 col-xs-12">
+                        <h1 className="text-xs-center">Sign In</h1>
+                        <p className="text-xs-center">
+                            <Link to="/register">Need an account?</Link>
+                        </p>
+
+                        {errorMessage.length ? 
+                        <ErrorMessage message={errorMessage} />
+                        : null
+                        }
+
+                        <form>
+                            <fieldset className="form-group">
+                                <input className="form-control form-control-lg" type="text" placeholder="Email" />
+                            </fieldset>
+                            <fieldset className="form-group">
+                                <input className="form-control form-control-lg" type="password" placeholder="Password" />
+                            </fieldset>
+                            <button className="btn btn-lg btn-primary pull-xs-right">
+                                Sign up
+                            </button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
