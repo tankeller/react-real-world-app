556329a2349f268f6bfd0041fbd92f27e06e1804
Tan Keller <ersoichso@gmail.com>
2019-10-05 14:12:26 +0200

    Sent register api request on submit

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from '@reach/router';
+import axios from 'axios';
 
 import ErrorMessage from '../components/errorMessage/ErrorMessage';
+import AuthContext from '../contexts/AuthContext';
 
 const SignUp = () => {
 
+    // eslint-disable-next-line
+    const [user, setUser] = useContext(AuthContext);
     const [errorMessage] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('https://conduit.productionready.io/api/users/',
+        {'user': {
+            'username': username,
+            'email': email,
+            'password': password
+        }})
+        .then((response) => {setUser(response.data.user)})
+        .catch((error) => {console.log(error)})
+    }
 
     return (
         <div className="auth-page">
@@ -20,15 +39,27 @@ const SignUp = () => {
 
                         <ErrorMessage message={errorMessage} />                        
 
-                        <form>
+                        <form onSubmit={(event) => {handleSubmit(event)}} >
                             <fieldset className="form-group">
-                                <input className="form-control form-control-lg" type="text" placeholder="Your Name" />
+                                <input 
+                                    onChange={(e) => {setUsername(e.target.value)}}
+                                    className="form-control form-control-lg" 
+                                    type="text" 
+                                    placeholder="Your Name" />
                             </fieldset>
                             <fieldset className="form-group">
-                                <input className="form-control form-control-lg" type="text" placeholder="Email" />
+                                <input 
+                                    onChange={(e) => {setEmail(e.target.value)}}
+                                    className="form-control form-control-lg"
+                                    type="text"
+                                    placeholder="Email" />
                             </fieldset>
                             <fieldset className="form-group">
-                                <input className="form-control form-control-lg" type="password" placeholder="Password" />
+                                <input 
+                                    onChange={(e) => {setPassword(e.target.value)}}
+                                    className="form-control form-control-lg"
+                                    type="password"
+                                    placeholder="Password" />
                             </fieldset>
                             <button className="btn btn-lg btn-primary pull-xs-right">
                                 Sign up
